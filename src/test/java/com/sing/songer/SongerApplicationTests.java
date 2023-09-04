abe0daf503b1e0178850b655f1c20c7a5312ac27
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-09-04 14:56:06 +0300

    updating readme and test cases

--- src/test/java/com/sing/songer/SongerApplicationTests.java
+++ src/test/java/com/sing/songer/SongerApplicationTests.java
@@ -1,13 +1,42 @@
 package com.sing.songer;
 
+import com.sing.songer.Model.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SongerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testGetTitle() {
+        Album album = new Album("Album Title", "Artist", 10, 60, "image.jpg");
+        assertEquals("Album Title", album.getTitle());
+    }
+
+    @Test
+    public void testGetArtist() {
+        Album album = new Album("Album Title", "Artist", 10, 60, "image.jpg");
+        assertEquals("Artist", album.getArtist());
+    }
+
+    @Test
+    public void testGetSongCount() {
+        Album album = new Album("Album Title", "Artist", 10, 60, "image.jpg");
+        assertEquals(10, album.getSongCount());
+    }
+
+    @Test
+    public void testGetLength() {
+        Album album = new Album("Album Title", "Artist", 10, 60, "image.jpg");
+        assertEquals(60, album.getLength());
+    }
+
+    @Test
+    public void testGetImageUrl() {
+        Album album = new Album("Album Title", "Artist", 10, 60, "image.jpg");
+        assertEquals("image.jpg", album.getImageUrl());
+    }
 
 }
